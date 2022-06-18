19f1f6b1c705496ce7e7a3856a4010d8e10d43d1
Lourini <61596340+Lourini@users.noreply.github.com>
2022-06-18 17:26:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, Response
 import cv2
 import numpy as np
-from keras.models import load_model
-from keras.preprocessing import image  
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image  
   
 
 #load model  
